a6bf2e7c695bf1ef100ecfc0386937517fa6fe4b
StaceyPeters01 <9018900@student.zadkine.nl>
2023-06-09 13:34:38 +0200

    Hobbies

---
+++ Python les 1/Hobbies.py
@@ -0,0 +1,12 @@
+hobby1 = input("Enter hobby 1:")
+print("Your hobby is: " + hobby1)
+
+hobby2 = input("Enter hobby 2:")
+print("Your hobby is: " + hobby2)
+
+hobby3 = input("Enter hobby 3:")
+print("Your hobby is: " + hobby3)
+
+thislist = [hobby1,hobby2,hobby3]
+
+print("Your three hobbies are",thislist)
